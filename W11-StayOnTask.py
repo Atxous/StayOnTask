4b1069ff0c6d5e0873236ad85b5c31028382ef6e
Atxous <83044757+Atxous@users.noreply.github.com>
2023-02-12 18:51:24 -0800

    Add files via upload

---
+++ W11-StayOnTask.py
@@ -0,0 +1,20 @@
+import webbrowser as web
+import schedule, os
+from ctypes import windll
+from time import sleep
+from playsound import playsound
+
+def close():
+    os.system("shutdown -t 0 -r -f")
+    return schedule.CancelJob
+
+def action():
+    playsound("C:\\Users\\jl255\\Downloads\\controversy.mp3")
+    schedule.every(5).minutes.do(close)
+    windll.user32.MessageBoxW(0, "\n\n      5 minutes remaining.      \n\n\n", "Calculus Time.", 0) 
+     
+schedule.every().day.at("19:00").do(action)
+
+while True:
+    schedule.run_pending()
+    sleep(60)
